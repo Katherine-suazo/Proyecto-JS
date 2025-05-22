be22e4c67f0c1693171a07ab8318d2c98226b553
Katherine <katherinesuazo032@gmail.com>
2025-05-22 19:15:41 -0400

    corregir punto btn

--- script.js
+++ script.js
@@ -33,8 +33,22 @@ function agregarEstudianteAlaTabla(student) {
     <td>${student.name}</td>
     <td>${student.lastName}</td>
     <td>${student.grade}</td>
+    <td> <button class="btn">Eliminar</button></td>
     `;
     tableBody.appendChild(row)
+
+    row.querySelector(".btn").addEventListener("click", function(){
+        borrarEstudiante(student, row)
+    })
+}
+
+function borrarEstudiante(student, row){
+    const index = students.indexOf(student);
+    if(index > -1){
+        students.splice(index, 1);
+        row.remove();
+        calcularPromedio();
+    }
 }
 
 const promDiv = document.getElementById("average")
